1c68fd7d568a5bb18a0ea3c51efe81e626c72105
EstefaniaDelgado <teff_64@hotmail.com>
2023-04-12 08:48:57 -0500

    responsive

--- src/components/CompError/CompError.jsx
+++ src/components/CompError/CompError.jsx
@@ -11,9 +11,9 @@ import {Link} from "react-router-dom"
     <div className={styles.container} >
       <div className={styles.errorInfo}>
         <h1 className={styles.title}>ERROR</h1>
-        <h1>4</h1>
+        <h1 className={styles.number}>4</h1>
           <img src={pokemonCat} alt="image_error" width={"200px"} height={"200px"}/>
-        <h1>4</h1>
+        <h1 className={styles.number}>4</h1>
       </div>
       <Link to={"/home"}>
       <button>Come Back Home!!</button>
